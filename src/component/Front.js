2fe26fd4aed6fc15fbb4e17df8bb205447198f26
123-Stephin <113153722+123-Stephin@users.noreply.github.com>
2023-07-17 10:16:19 +0530

    Front.js

--- src/component/Front.js
+++ src/component/Front.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Front() {
   return (
     <div>
-      <h1>This is my front page</h1>
+      <h1 style={{ justifyContent: 'center', alignItems:"center"}}>This is my Front Page</h1>
+      <img src={require("../public/abc.jpg")} alt='nba' />
     </div>
   )
 }
