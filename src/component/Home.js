28e4c755b1e46f15884aab39e1002a2fe0501e63
123-Stephin <113153722+123-Stephin@users.noreply.github.com>
2023-07-17 10:17:11 +0530

    Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,16 +1,40 @@
-import React from 'react'
-// import Firebase from './Firebase'
+import { useEffect, useState } from 'react';
+import './Home.css'
 
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://project-bd1df-default-rtdb.firebaseio.com/.json')
+      .then(response => response.json())
+      .then(data => {
+        const dataArray = [];
+        for (const key in data) {
+          dataArray.push({
+            id: key,
+            name: data[key].name,
+            age: data[key].age,
+            domain: data[key].domain
+          });
+        }
+        setData(dataArray);
+      });
+  }, []); 
+
   return (
-    <div>
-      <h1>
-      welcome home
-      </h1>
-      {/* <Firebase /> */}
+    <div className='fire-base' >
+      <h2  className='title'>Data from Firebase</h2>
+      <div className='card'>
+        {data.map(item => (
+          <div className='card-display' key={item.id}>
+            <h3>{item.name}</h3>
+            <p>Age: {item.age}</p>
+            <p>Domain: {item.domain}</p>
+          </div>
+        ))}
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
